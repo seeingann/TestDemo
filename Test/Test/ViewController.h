7a4502d5abc248feb17a9ee119e628a840390155
Cici <tangtang@tangtang.local>
2016-07-05 13:33:50 +0800

    First Fixing

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by tangtang on 16/7/5.
+//  Copyright © 2016年 huaren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by tangtang on 16/7/5.
+//  Copyright © 2016年 huaren. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"Hello...");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
